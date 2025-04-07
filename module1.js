49467c198532730705d9164795f8abef5c72ad7d
Jerry Huang <17321421572@qq.com>
2025-04-07 14:13:18 +0800

    JavaScript_files

---
+++ module1.js
@@ -0,0 +1,5 @@
+export function sayHi(user): {
+  alert(`Hello, ${user}!`);
+}
+
+alert("module1.js is loaded");
---
+++ text.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>我的网页</title>
+    </head>
+    <body>
+        <h1>欢迎来到我的网页</h1>
+        <p>这是一个示例网页。</p>
+        <a href="https://www.example.com">访问示例网站</a>
+    </body>
+</html>
